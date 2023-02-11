2581d186c8eb533f91267d1c17a98bd722318f9e
wassim chawch <104462542+WASSIMCHAWCH@users.noreply.github.com>
2023-02-11 14:15:29 +0100

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -5,9 +5,11 @@ pragma solidity ^0.8.17;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract Wassim is ERC20("Wassim","WS")  {
+            address payable owner ;
 
-            constructor () {
+            constructor () payable {
                 _mint (msg.sender , 10000000 * (10 ** decimals()));
+                owner = payable(msg.sender) ;
     }
 
 }
